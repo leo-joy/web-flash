f41e1ae3cf7eb5a7aea0a6488f90d7e45d8d6f30
leo-joy <small_ant@sina.cn>
2020-06-16 09:49:32 +0800

    修改接口服务的数据库配置

--- node-interface/src/config/config.js
+++ node-interface/src/config/config.js
@@ -1,12 +1,11 @@
-//配置mongodb数据库
+//配置mysql数据库
 module.exports = {
-
 	mysqldb: {
 		connectionLimit: 10000,
 		connectTimeout  : 60 * 60 * 1000,
 	 	aquireTimeout   : 60 * 60 * 1000,
 		timeout         : 60 * 60 * 1000,
-		host: '10.1.10.35',
+		host: 'mysqlFR',
 		user: 'root',
 		//port     : 3306,
 		password: 'dp2015',
@@ -15,9 +14,22 @@ module.exports = {
 	},
 
 	// mysqldb: {
-	// 	connectionLimit: 1000,
+	// 	connectionLimit: 10000,
 	// 	connectTimeout  : 60 * 60 * 1000,
 	//  	aquireTimeout   : 60 * 60 * 1000,
+	// 	timeout         : 60 * 60 * 1000,
+	// 	host: '10.1.10.35',
+	// 	user: 'root',
+	// 	//port     : 3306,
+	// 	password: 'dp2015',
+	// 	database: 'web-flash',
+	// 	autoReconnect: true
+	// },
+
+	// mysqldb: {
+	// 	connectionLimit: 1000,
+	// 	connectTimeout  : 60 * 60 * 1000,
+	//  aquireTimeout   : 60 * 60 * 1000,
 	// 	timeout         : 60 * 60 * 1000,
 	// 	host: '127.0.0.1',
 	// 	user: 'root',
