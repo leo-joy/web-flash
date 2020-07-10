cd7713302413c991a03f55e00d1c03ac64e55eab
leo-joy <small_ant@sina.cn>
2020-07-10 21:11:56 +0800

    测试服务器脚本添加

--- restart.sh
+++ restart.sh
@@ -24,10 +24,10 @@ docker start mysqlFR tomcatFR nginxFR nodeFRInterface
 #docker run -d -p 3306:3306 --privileged=true --name mysqlFR  -v $PWD/data/mysql:/etc/mysql  -e MYSQL_ROOT_PASSWORD=dp2015  mysql:5.7
 
 #2、创建tomcat 容器
-#docker run -itd -p 8080:8082 --privileged=true --link mysqlZSYJL:mysqlcontainer --name tomcatFR -v $PWD/data/runtime:/home/user/faren/data/runtime -v $PWD/service/java/webapps/faren:/usr/local/tomcat/webapps  tomcat:9.0
+#docker run -itd -p 8090:8082 --privileged=true --link mysqlFR:mysqlcontainer --name tomcatFRTEST -v $PWD/data/runtime:/home/user/faren/data/runtime -v $PWD/service/java/webapps:/usr/local/tomcat/webapps  tomcat
 
 #3、创建nginx容器
-#docker run -d -p 4010:80 --privileged=true --name nginxFR  -v $PWD/client/faren/dist:/usr/share/nginx/html  nginx:1.17.3
+#docker run -d -p 4010:80 --privileged=true --name nginxFRTEST  -v $PWD/client/dist:/usr/share/nginx/html  nginx:1.17.3
 
 #4、接口服务
 #docker run -itd -p 5008:3000 --privileged=true --name nodeFRInterface -v $PWD/service/nodeInterface:/usr/src/app -w /usr/src/app  node:10.16.3 npm start
