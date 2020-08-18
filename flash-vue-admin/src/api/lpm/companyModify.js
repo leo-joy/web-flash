8756dd949ad117569cb4e78d5cd48f691957f969
leo-joy <small_ant@sina.cn>
2020-08-18 14:25:46 +0800

    企业变更中post请求，参数在url中不要传输

--- flash-vue-admin/src/api/lpm/companyModify.js
+++ flash-vue-admin/src/api/lpm/companyModify.js
@@ -1,28 +1,29 @@
 import request from '@/utils/request'
+import Qs from 'qs'
 
 export function getList(params) {
-    return request({
-        url: '/lpm/company/modify/list',
-        method: 'get',
-        params
-    })
+  return request({
+    url: '/lpm/company/modify/list',
+    method: 'get',
+    params
+  })
 }
 
-
 export function save(params) {
-    return request({
-        url: '/lpm/company/modify',
-        method: 'post',
-        params
-    })
+  return request({
+    url: '/lpm/company/modify',
+    method: 'post',
+    data: Qs.stringify(params) // 解决url中参数过长导致的414错误
+
+  })
 }
 
 export function remove(id) {
-    return request({
-        url: '/lpm/company/modify',
-        method: 'delete',
-        params: {
-            id: id
-        }
-    })
+  return request({
+    url: '/lpm/company/modify',
+    method: 'delete',
+    params: {
+      id: id
+    }
+  })
 }
