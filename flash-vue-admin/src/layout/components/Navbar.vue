2bd1268f551591f7a86296e353631e10d49d1174
leo-joy <small_ant@sina.cn>
2020-08-21 14:41:17 +0800

    注释导航链接和左侧导航收起打开样式

--- flash-vue-admin/src/layout/components/Navbar.vue
+++ flash-vue-admin/src/layout/components/Navbar.vue
@@ -74,8 +74,8 @@ export default {
     line-height: 50px;
     border-radius: 0px !important;
     .hamburger-container {
-      line-height: 58px;
-      height: 50px;
+      line-height: 45px;
+      height: 45px;
       float: left;
       padding: 0 10px;
     }
