a1954a0e93e8fd9ef59473db570c3676c3c86316
leo-joy <small_ant@sina.cn>
2020-08-23 20:30:32 +0800

    股权架构图页面调整

--- flash-vue-admin/public/guquan/files/siteconfig.js
+++ flash-vue-admin/public/guquan/files/siteconfig.js
@@ -1,5 +1,9 @@
+/**
+ * 股权图通用变量配置
+ */
 var Duohuo ={};
-INDEX_URL="/dev-api"; 
+//INDEX_URL="/dev-api"; 
+INDEX_URL="/prod-api"; 
 INDEX_URL_A=""; 
 INDEX_ROOT="";
 CMS_STYLE_ROOT="/material/theme/chacha/cms";
@@ -8,5 +12,5 @@ var domainStr = window.location.host;
 var domainArr = domainStr.split('.');
 var subDomain = domainArr[0];
 if(subDomain === 'pinpai'){
-    INDEX_URL_A = 'https://www.qichacha.com';
+    INDEX_URL_A = 'https://faren.agile.com.cn';
 }
